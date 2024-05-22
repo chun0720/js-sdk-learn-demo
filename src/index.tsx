583c31efcf5eb4dcd47daf78b3cef2d26fd841ea
chun0720 <mouse0720@88.com>
2024-05-22 09:04:27 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -14,3 +14,18 @@ useEffect(() => {
   };
   fn();
 }, []);
+
+const formatFieldMetaList = (metaList: ICurrencyFieldMeta[]) => { 
+  return metaList.map(meta => ({ label: meta.name, value: meta.id })); 
+}; 
+
+return <div>
+  <div style={{ margin: 10 }}> 
+    <div>Select Field</div> 
+    <Select style={{ width: 120 }} onSelect={setSelectFieldId} options={formatFieldMetaList(currencyFieldMetaList)}/> 
+  </div> 
+  <div style={{ margin: 10 }}>
+    <div>Select Currency</div>
+    <Select options={CURRENCY} style={{ width: 120 }} onSelect={setCurrency}/>
+  </div>
+</div>
