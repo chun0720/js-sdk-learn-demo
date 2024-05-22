3a6e6f5596a3dfa691303258dcca91dd3d14fb92
chun0720 <mouse0720@88.com>
2024-05-22 09:04:03 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,3 +5,12 @@ function LoadApp() {
   const [currencyFieldMetaList, setMetaList] = useState<ICurrencyFieldMeta[]>([]) 
   const [selectFieldId, setSelectFieldId] = useState<string>(); 
   const [currency, setCurrency] = useState<CurrencyCode>(); 
+
+useEffect(() => {
+  const fn = async () => {
+    const table = await bitable.base.getActiveTable();
+    const fieldMetaList = await table.getFieldMetaListByType<ICurrencyFieldMeta>(FieldType.Currency); 
+    setMetaList(fieldMetaList); 
+  };
+  fn();
+}, []);
